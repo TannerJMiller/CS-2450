bc5fef40468532a1663553e0cea23c6ab38bbe26
Tanner M. <robofusedude@gmail.com>
2026-08-27 13:08:46 -0600

    added higher and lower asking

--- age_guess.py
+++ age_guess.py
@@ -3,13 +3,20 @@ import random
 def guess_age():
     name = input("Enter your name: ")
     got_age = False
+    c = 15
+    d = 40
     while not got_age:
-        b = random.randint(15, 40)
+        b = random.randint(c, d)
         a = input(f"Is your age {b}? (y/n): ")
         if a.lower() == 'y':
             print(f"Great! I guessed your age, {name}. You are {b} years old.")
             got_age = True
         else:
-            print("Rats!")
+            e =input("Rats! is your age higher or lower than my guess? (h/l): ")
+            if e == "h":
+                c = b
+            elif e == "l":
+                d = b
+
 
 guess_age()
